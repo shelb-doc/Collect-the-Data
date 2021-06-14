48d18846b44334ce19dbbce2bd79b4375da1a8b9
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-06-14 11:00:42 -0400

    starter files for backend

---
+++ collectthedata/src/main/java/com/mimikyu/collectthedata/CollectthedataApplication.java
@@ -0,0 +1,13 @@
+package com.mimikyu.collectthedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectthedataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectthedataApplication.class, args);
+	}
+
+}
